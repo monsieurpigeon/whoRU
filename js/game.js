0ec89f1b79ec8862308c81fc4ac83472dedff504
monsieurpigeon <mpigeon@student.42.fr>
2017-06-08 19:51:21 +0200

    premiere partie avec bbeldame

--- js/game.js
+++ js/game.js
@@ -8,12 +8,13 @@ Game.Preloader.prototype = {
         this.scale.pageAlignHorizontally = true;
     }, 
     preload: function() {
-        this.load.path = 'assets/';
-        game.load.image('mpigeon', 'small_mpigeon.jpg');
+		this.load.path = 'assets/';
+		game.load.json('students', 'students.json');
         console.log('preload');
     },
     create: function() {
 		this.state.start('Game.Welcome');
+		Game.students = game.cache.getJSON('students');
 	}
 };
 
@@ -72,26 +73,47 @@ Game.MainGame = function(){};
 Game.MainGame.prototype = {
     preload: function () {
         console.log('maingame');
+		game.load.path = '';
+		game.load.baseURL = 'https://cdn.intra.42.fr/users/';
+    	game.load.crossOrigin = 'anonymous';
+		Game.students.login.forEach(function(element) {
+			game.load.image(element, 'small_'+element+'.jpg');
+		});
+        
 	},
 	create: function () {
-        for (i = 0; i < 6; i++) {
-            for (j = 0; j < 4; j++) {
-                var newtile = this.add.sprite(this.world.centerX + 134 * (i - 2.5) - 230,this.world.centerY + 178 * (j - 1.5), 'mpigeon');
-                newtile.anchor.x = 0.5;
-                newtile.anchor.y = 0.5;
+        for (j = 0; j < 4; j++) {
+            for (i = 0; i < 6; i++) {
+                var newtile = this.add.sprite(this.world.centerX + 134 * (i - 2.5) - 230,this.world.centerY + 178 * (j - 1.5), Game.students.login[j * 6 + i]);
+				var text = game.add.text(this.world.centerX + 134 * (i - 2.5) - 285, this.world.centerY + 178 * (j - 1.5) + 50, Game.students.login[j * 6 + i], {
+					font: "20px Arial",
+					fill: "#ffffff",
+					align: "center"
+				});
+                newtile.anchor.setTo(0.5, 0.5);
                 newtile.inputEnabled = true;
+				cropRect = new Phaser.Rectangle(newtile.width/2 - 55, newtile.height/2 - 75, 120, 162);
+				newtile.crop(cropRect);
 				newtile.events.onInputOver.add(this.over, this);
 				newtile.events.onInputOut.add(this.out, this);
 				newtile.events.onInputDown.add(this.click, this);
             }
         }
-		this.time.events.add(3000, this.next, this);
+		//this.time.events.add(3000, this.next, this);
 	},
     over: function (item) {
+		item.anchor.setTo(0.52, 0.52);
 	},
 	out: function (item) {
+		item.anchor.setTo(0.5, 0.5);
 	},
 	click: function (item) {
+		if (item.tint == 0xff0000) {
+			item.tint = 0xffffff;
+		} else {
+			item.tint = 0xff0000;
+		}
+		
 	},
 	next: function () {
 		this.state.start('Game.Result');
